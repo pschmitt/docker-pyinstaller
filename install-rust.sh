60feaf1e3918bad6f4c01f7cf87c1400b9afe29a
Philipp Schmitt <philipp@schmitt.co>
2021-04-21 14:12:04 +0200

    Fix rust toolchain arch for i386

--- install-rust.sh
+++ install-rust.sh
@@ -7,10 +7,20 @@ case "$(uname -m)" in
     ;;
 esac
 
+EXTRA_ARGS=()
+
+# Check if we are inside a i386 image and set the rust toolchain accordingly
+# When inside an i386 container on an amd64 host uname -m will return x64_64
+# hence the file check
+if file /usr/bin/file | grep -q "Intel 80386"
+then
+  EXTRA_ARGS+=(--default-host i686-unknown-linux-gnu)
+fi
+
 # Where to put the symlinks
 DEST=/usr/local/bin
 
-curl https://sh.rustup.rs -sSf | sh -s -- -y
+curl https://sh.rustup.rs -sSf | sh -s -- -y "${EXTRA_ARGS[@]}"
 for file in "${HOME}"/.cargo/bin/*;
 do
   ln -sfv "$file" "$DEST"
